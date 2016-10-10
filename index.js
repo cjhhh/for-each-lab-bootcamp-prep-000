8e757b486d60e7ac12a119afd46c95bbc0c9f2c3
Tayari Appiah <cjhhh.ta@gmail.com>
2016-10-10 14:13:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function iterativeLog(array){
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback){
+   const arr = [1, 2, 3, 4, 5]
+    arr.forEach((callback) => {
+        return arr
+    })
+}
